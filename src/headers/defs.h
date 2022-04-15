57bc6a7616a959e9536323e002aaa4c40dc76fc3
wojciechsi <redocean@protonmail.com>
2022-04-15 15:11:10 +0200

    refaktoryzacja

--- src/headers/defs.h
+++ src/headers/defs.h
@@ -7,4 +7,6 @@
 constexpr unsigned short SCREEN_HEIGHT = 256;
 constexpr unsigned short SCREEN_WIDTH = 240;
 
+constexpr unsigned short MAX_LEVEL_SIZE = 1000;
+
 constexpr double FRAME_TIME = 0.017;
