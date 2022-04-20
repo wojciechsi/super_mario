b33b04896e652a56b1d9234f2c325c5277d7369d
tomazaw <tomazaw886@student.polsl.pl>
2022-04-20 14:30:10 +0200

    Mario sie odwraca

--- src/Mario.cpp
+++ src/Mario.cpp
@@ -10,12 +10,16 @@ void Mario::MoveStatus()
     goesRight = false;
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
     {
-        //nie wypuść ne lewo od ekranu
+        //nie wypuść ne lewo od ekran
+        this->sprite.setScale(-1.0f, 1.0f);
         if(this->sprite.getPosition().x < (this->size.x)/2) return;
         else this-> move(-1.0f, 0);
+
+
     }
     else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
     {
+        this->sprite.setScale(1.0f, 1.0f);
         if(this->sprite.getPosition().x < SCREEN_WIDTH/2)
             this-> move(1.0f, 0);
         else goesRight = true;
