63980d073917e438bb73abe241a7effbc45c62a4
tomazaw <tomazaw886@student.polsl.pl>
2022-06-01 20:23:11 +0200

    Naprawiona czcionka

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -13,35 +13,40 @@ Menu::Menu(float width, float height)
     mainMenu[0].setFont(font);
     mainMenu[0].setFillColor(sf::Color::White);
     mainMenu[0].setString("Nowa Gra");
-    mainMenu[0].setCharacterSize(0.06*SCREEN_WIDTH);
+    mainMenu[0].setCharacterSize(0.6*SCREEN_WIDTH);
+    mainMenu[0].setScale(0.1, 0.1);
     mainMenu[0].setPosition(0.5*SCREEN_WIDTH, 0.3*SCREEN_HEIGHT);
 
     //continue
     mainMenu[1].setFont(font);
     mainMenu[1].setFillColor(sf::Color::White);
     mainMenu[1].setString("Kontynuuj");
-    mainMenu[1].setCharacterSize(0.06*SCREEN_WIDTH);
+    mainMenu[1].setCharacterSize(0.6*SCREEN_WIDTH);
+    mainMenu[1].setScale(0.1, 0.1);
     mainMenu[1].setPosition(0.5*SCREEN_WIDTH, 0.4*SCREEN_HEIGHT);
 
     //options
     mainMenu[2].setFont(font);
     mainMenu[2].setFillColor(sf::Color::White);
     mainMenu[2].setString("Opcje");
-    mainMenu[2].setCharacterSize(0.06*SCREEN_WIDTH);
+    mainMenu[2].setCharacterSize(0.6*SCREEN_WIDTH);
+    mainMenu[2].setScale(0.1, 0.1);
     mainMenu[2].setPosition(0.5*SCREEN_WIDTH, 0.5*SCREEN_HEIGHT);
 
     //Creators
     mainMenu[3].setFont(font);
     mainMenu[3].setFillColor(sf::Color::White);
     mainMenu[3].setString("Tworcy");
-    mainMenu[3].setCharacterSize(0.06*SCREEN_WIDTH);
+    mainMenu[3].setCharacterSize(0.6*SCREEN_WIDTH);
+    mainMenu[3].setScale(0.1, 0.1);
     mainMenu[3].setPosition(0.5*SCREEN_WIDTH, 0.6*SCREEN_HEIGHT);
 
     //exit game
     mainMenu[4].setFont(font);
     mainMenu[4].setFillColor(sf::Color::White);
     mainMenu[4].setString("Wyjdz z gry");
-    mainMenu[4].setCharacterSize(0.06*SCREEN_WIDTH);
+    mainMenu[4].setCharacterSize(0.6*SCREEN_WIDTH);
+    mainMenu[4].setScale(0.1, 0.1);
     mainMenu[4].setPosition(0.5*SCREEN_WIDTH, 0.7*SCREEN_HEIGHT);
 
     selected = -1;
